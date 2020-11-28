02d2d89f6e57f71b212f1ae668fd80471b399037
pinnci <david.kopacik@gmail.com>
2020-11-28 12:31:28 +0100

    Added more responsibility to views

--- src/Components/PortfolioItemSlider.js
+++ src/Components/PortfolioItemSlider.js
@@ -57,7 +57,22 @@ function PortfolioItemSlider(){
         autoplay:false,
         slidesToShow:3,
         slidesToScroll:1,
-
+        responsive: [
+            {
+              breakpoint: 1100,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+        ]
     }
 
     return(
